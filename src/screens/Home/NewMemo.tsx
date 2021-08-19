c54c22f4264efbf5589b88d7bdc1bf7d51b7593f
Youssef Tarek <yousseftarek@live.com>
2021-08-19 22:50:08 +0200

    trial for video compressor

--- src/screens/Home/NewMemo.tsx
+++ src/screens/Home/NewMemo.tsx
@@ -10,6 +10,7 @@ const { height: HEIGHT } = Dimensions.get("window");
 import useVideos from "../../hooks/useVideos";
 import usePermissions from "../../hooks/usePermissions";
 import Permissions from "./Permissions";
+import { Video as VideoCompressor } from "react-native-compressor";
 
 const VIDEO_HEIGHT = HEIGHT / 5;
 
@@ -20,6 +21,7 @@ type Props = {
 const NewMemo = ({ selected, onSelect }: Props) => {
   const { loading, memo, updateMemo, createVideo } = useVideos();
 
+  const [compressingProgress, setCompressingProgress] = useState(0);
   const [progress, setProgress] = useState(0);
   const [uploading, setUploading] = useState(false);
   const { hasPermissions } = usePermissions();
@@ -31,6 +33,7 @@ const NewMemo = ({ selected, onSelect }: Props) => {
     });
     return response;
   }, []);
+  console.log({ compressingProgress });
 
   const onCreateVideo = useCallback(() => {
     createVideo(memo);
@@ -38,6 +41,17 @@ const NewMemo = ({ selected, onSelect }: Props) => {
 
   const processVideo = useCallback(
     async (memo): Promise<ImagePicker.ImagePickerResult> => {
+      const result = await VideoCompressor.compress(
+        "file://path_of_file/BigBuckBunny.mp4",
+        {
+          compressionMethod: "auto",
+        },
+        (progress) => {
+          setCompressingProgress(progress);
+        }
+      );
+      console.log({ result });
+
       return memo;
     },
     []
@@ -51,7 +65,7 @@ const NewMemo = ({ selected, onSelect }: Props) => {
 
     if (result.cancelled) return;
     const thumbnail = await getThumbnail(result);
-    // result = await processVideo(result);
+    result = await processVideo(result);
     if (thumbnail.uri) updateMemo({ ...result, thumbnail: thumbnail.uri });
   }, []);
 
